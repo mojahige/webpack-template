3d2bafd70cbe38e8d0475fa3cdb294ac2874658d
YuNozato <mojahige@gmail.com>
2017-07-12 02:13:54 +0900

    js と css の設定を別々にした。

--- webpack.config.js
+++ webpack.config.js
@@ -3,57 +3,73 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpackSetting = require('./webpack.setting');
-const bundleCSS = new ExtractTextPlugin({ filename: 'assets/dist/stylesheets/[name].bundle.css', allChunks: true });
 
-module.exports = {
-  context: path.resolve(__dirname, './htdocs/assets/src/javascripts'),
-  entry: {
-    app: './app.js',
-  },
-  resolve: {
-    modules: [
-      __dirname + '/node_modules'
-    ],
-    alias: {
-      src: __dirname + '/htdocs/assets/src/javascripts',
+module.exports = [
+  {
+    context: path.resolve(__dirname, './htdocs/assets/src/javascripts'),
+    entry: {
+      app: './app.js',
+    },
+    resolve: {
+      modules: [
+        __dirname + '/node_modules'
+      ],
+      alias: {
+        src: __dirname + '/htdocs/assets/src/javascripts',
+      }
+    },
+    output: {
+      path: __dirname + '/htdocs/',
+      publicPath: '/',
+      filename: 'assets/dist/javascripts/[name].bundle.js'
+    },
+    devServer: {
+      contentBase: 'htdocs',
+      inline: true
+      // hot: true
+    },
+    devtool: process.env.NODE_ENV === 'production' ? '': 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [{
+            loader: 'babel-loader',
+            options: { presets: [
+              [ 'es2015', { modules: false } ]
+            ]}
+          }]
+        }
+      ]
     }
   },
-  output: {
-    // path: __dirname + '/htdocs/',
-    publicPath: '/',
-    filename: 'assets/dist/javascripts/[name].bundle.js'
-  },
-  devServer: {
-    contentBase: 'htdocs',
-    inline: true
-    // hot: true
-  },
-  devtool: process.env.NODE_ENV === 'production' ? '': 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: { presets: [
-            [ 'es2015', { modules: false } ]
-          ]}
-        }]
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use:  ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: process.env.NODE_ENV === 'production' ? webpackSetting.css.production: webpackSetting.css.develop
-        }),
-      }
+  {
+    context: path.resolve(__dirname, './htdocs/assets/src/stylesheets'),
+    entry: {
+      style: './_hoge.scss',
+    },
+    output: {
+      path: __dirname + '/htdocs/',
+      publicPath: '/',
+      filename: 'assets/dist/stylesheets/[name].bundle.css'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sass|scss)$/,
+          use:  ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: process.env.NODE_ENV === 'production' ? webpackSetting.css.production: webpackSetting.css.develop
+          }),
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin({
+        filename: 'assets/dist/stylesheets/[name].bundle.css',
+        allChunks: true
+      })
     ]
-  },
-  plugins: [
-    bundleCSS,
-    new HtmlWebpackPlugin({
-      template: path.resolve('htdocs/index.html')
-    })
-  ]
-};
+  }
+];
